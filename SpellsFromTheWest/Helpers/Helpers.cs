5e3fb5ad128c2c9855dcf0543d11b3c3bd581bea
wilhelm <36527456+wilhelmw201@users.noreply.github.com>
2026-05-01 23:34:48 +0200

    rename Event because of naming ambiguity

--- SpellsFromTheWest/Helpers/Helpers.cs
+++ SpellsFromTheWest/Helpers/Helpers.cs
@@ -1,4 +1,5 @@
-﻿using GameData.Domains.Combat;
+﻿using GameData.Domains.Character;
+using GameData.Domains.Combat;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,29 @@ namespace MoreFactionCombatSkillsBackend.Helpers
         {
             var tricks = combatChar.GetTricks();
             return tricks.Tricks.Sum((p) => (p.Value == trick ? 1 : 0));
+        }
+
+        public static OuterAndInnerInts Multiply(OuterAndInnerInts baseInts, double multiplier)
+        {
+            return new OuterAndInnerInts
+            {
+                Outer = (int)(baseInts.Outer * multiplier),
+                Inner = (int)(baseInts.Inner * multiplier)
+            };
+        }
+
+        public static HitOrAvoidInts Multiply(HitOrAvoidInts baseInts, double multiplier)
+        {
+            return new HitOrAvoidInts
+            (
+                new int[4]
+                {
+                    (int)(baseInts[0] * multiplier),
+                    (int)(baseInts[1] * multiplier),
+                    (int)(baseInts[2] * multiplier),
+                    (int)(baseInts[3] * multiplier)
+                }
+            );
         }
     }
 }
